890780a85d3fe41cebc8a399f82788c8b902c0c3
Matt McGee <mlmcgeenc@gmail.com>
2022-02-07 16:22:21 -0500

    set up concurrently as script 'dev'
    to compile/watch/run use 'npm run dev'

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,7 @@
-// npx nodemon -w dist -q dist/index.js
 "use strict";
 function multiply(a, b) {
     return a * b;
 }
-const a = 4;
+const a = 8;
 const b = 2;
 console.log(`${a} * ${b} = ${multiply(a, b)}`);
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ function multiply(a: number, b: number): number {
   return a * b
 }
 
-const a = 4
+const a = 8
 const b = 2
 
 console.log(`${a} * ${b} = ${multiply(a, b)}`)
