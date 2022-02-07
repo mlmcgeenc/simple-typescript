e53523c6d0b6ec24b0d622d95f0724d24c9e0edf
Matt McGee <mlmcgeenc@gmail.com>
2022-02-07 17:03:31 -0500

    set up multiply module

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,27 @@
 "use strict";
-function multiply(a, b) {
-    return a * b;
-}
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const multiply_1 = __importStar(require("./multiply"));
 const a = 8;
 const b = 2;
-console.log(`${a} * ${b} = ${multiply(a, b)}`);
+const c = 3;
+console.log(`${a} * ${b} = ${(0, multiply_1.default)(a, b)}`);
+console.log(`${c} * two = ${(0, multiply_1.multiplyByTwo)(c)}`);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-function multiply(a: number, b: number): number {
-  return a * b
-}
+import multiply, {multiplyByTwo as mBy2} from './multiply'
 
 const a = 8
 const b = 2
+const c = 3
 
 console.log(`${a} * ${b} = ${multiply(a, b)}`)
+console.log(`${c} * two = ${mBy2(c)}`)
